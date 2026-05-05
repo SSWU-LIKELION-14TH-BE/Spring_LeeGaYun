54d2bdce9e54d8bafbac327b2468319ff74eaea1
riveryunny <gayunphone@gmail.com>
2026-05-05 22:15:54 +0900

    implement calculator operations

--- src/main/java/org/example/session222/CalculatorAddRequest.java
+++ src/main/java/org/example/session222/CalculatorAddRequest.java
@@ -1,19 +1,22 @@
 package org.example.session222;
 
 public class CalculatorAddRequest {
-    private final int number1;
-    private final int number2;
+    private int number1;
+    private int number2;
+
+    public CalculatorAddRequest() {
+    }
 
     public CalculatorAddRequest(int number1, int number2) {
         this.number1 = number1;
         this.number2 = number2;
     }
 
-    public int getNumber1(){
+    public int getNumber1() {
         return number1;
     }
 
-    public int getNumber2(){
+    public int getNumber2() {
         return number2;
     }
 }
